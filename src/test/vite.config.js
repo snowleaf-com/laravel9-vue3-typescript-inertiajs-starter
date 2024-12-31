2960e949e02174d12eac3d4df5ba7e7403a987a8
snowleaf-com <yy.dec5@gmail.com>
2024-12-31 15:42:29 +0900

    Initial commit

---
+++ src/test/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.ts',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    server: {
+    host: '0.0.0.0',
+    watch: {
+        usePolling: true,
+    },
+    port:5174,
+}
+});
